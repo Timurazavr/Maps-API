60fb38be1bd6f5f45a2e321b8514d2164e72ec2d
Timur <timur.garipov11@gmail.com>
2025-03-09 16:52:36 +0300

    Большая задача по Maps API. Часть №5

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ class Example(QMainWindow):
         self.z = 15
         self.theme = "light"
         self.themeBtn.clicked.connect(self.change_theme)
-        self.searchBtn.clicked.connect(lambda: self.get_coords(self.searchEdit.text()))
+        self.searchBtn.clicked.connect(self.get_coords)
         self.themeBtn.setFocusPolicy(QtCore.Qt.FocusPolicy.ClickFocus)
         self.getImage()
 
@@ -27,9 +27,9 @@ class Example(QMainWindow):
             "theme": self.theme,
         }
         if search is not None:
-            self.lon, self.lat = search
-            params['pt'] = f'{self.lon},{self.lat},pm2rdm'
-        params['ll'] = ",".join((str(self.lon), str(self.lat)))
+            self.lon, self.lat = map(float, search)
+            params["pt"] = f"{self.lon},{self.lat},pm2rdm"
+        params["ll"] = ",".join((str(self.lon), str(self.lat)))
         response = requests.get(api_server, params=params)
         if not response:
             print("Ошибка выполнения запроса:")
@@ -54,39 +54,46 @@ class Example(QMainWindow):
                 self.z += 1
                 self.getImage()
         elif event.key() == QtCore.Qt.Key.Key_Up:
-            self.lat += 90 / 2 ** self.z
+            self.lat += 90 / 2**self.z
             if self.lat >= 85:
                 self.lat = -85
             self.getImage()
         elif event.key() == QtCore.Qt.Key.Key_Down:
-            self.lat -= 90 / 2 ** self.z
+            self.lat -= 90 / 2**self.z
             if self.lat <= -85:
                 self.lat = 85
             self.getImage()
         elif event.key() == QtCore.Qt.Key.Key_Left:
-            self.lon -= 180 / 2 ** self.z
+            self.lon -= 180 / 2**self.z
             if self.lon <= -180:
                 self.lon = 180
             self.getImage()
         elif event.key() == QtCore.Qt.Key.Key_Right:
-            self.lon += 180 / 2 ** self.z
+            self.lon += 180 / 2**self.z
             if self.lon >= 180:
                 self.lon = -180
             self.getImage()
         elif event.key() == 16777220:
-            self.get_coords(self.searchEdit.text())
+            self.get_coords()
 
     def change_theme(self):
         self.theme = "light" if self.theme == "dark" else "dark"
-        self.getImage()
         self.setFocus()
+        self.getImage()
 
-    def get_coords(self, place):
-        api_key = '8013b162-6b42-4997-9691-77b7074026e0'
+    def get_coords(self):
+        place = self.searchEdit.text()
+        api_key = "8013b162-6b42-4997-9691-77b7074026e0"
         geocoder_request = f"http://geocode-maps.yandex.ru/1.x/?apikey={api_key}&geocode={place}&format=json"
-        response = requests.get(geocoder_request).json()['response']['GeoObjectCollection']['featureMember'][0][
-            'GeoObject']
-        self.getImage(response['Point']['pos'].split())
+        response = requests.get(geocoder_request)
+        try:
+            result = response.json()["response"]["GeoObjectCollection"][
+                "featureMember"
+            ][0]["GeoObject"]["Point"]["pos"]
+            self.getImage(result.split())
+        except Exception:
+            pass
+        self.setFocus()
 
 
 if __name__ == "__main__":
