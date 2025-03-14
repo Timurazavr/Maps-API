1bf7e737d69024639908bc81d412b37993b73161
Timur <timur.garipov11@gmail.com>
2025-03-14 17:40:50 +0300

    Большая задача по Maps API. Часть №8

--- main.py
+++ main.py
@@ -14,12 +14,15 @@ class Example(QMainWindow):
         self.z = 15
         self.theme = "light"
         self.themeBtn.clicked.connect(self.change_theme)
-        self.themeBtn.setFocusPolicy(QtCore.Qt.FocusPolicy.ClickFocus)
         self.searchBtn.clicked.connect(self.get_coords)
         self.searchEdit.setText("Московский Кремль")
         self.resetBtn.clicked.connect(self.reset)
+        self.setFocusPolicy(QtCore.Qt.FocusPolicy.StrongFocus)
+        self.themeBtn.setFocusPolicy(QtCore.Qt.FocusPolicy.NoFocus)
+        self.searchBtn.setFocusPolicy(QtCore.Qt.FocusPolicy.NoFocus)
+        self.resetBtn.setFocusPolicy(QtCore.Qt.FocusPolicy.NoFocus)
+        self.searchEdit.setFocusPolicy(QtCore.Qt.FocusPolicy.ClickFocus)
         self.get_coords()
-        self.search = ()
 
     def getImage(self):
         api_server = "https://static-maps.yandex.ru/v1"
@@ -30,7 +33,10 @@ class Example(QMainWindow):
             "theme": self.theme,
         }
         if self.search != ():
-            params['pt'] = ",".join((*map(str, self.search), "pm2rdm")),
+            params["pt"] = (",".join((*map(str, self.search), "pm2rdm")),)
+            self.resultLbl.setText(self.adress)
+        else:
+            self.resultLbl.setText("")
         response = requests.get(api_server, params=params)
         if not response:
             print("Ошибка выполнения запроса:")
@@ -40,8 +46,9 @@ class Example(QMainWindow):
         with open(self.map_file, "wb") as file:
             file.write(response.content)
         self.pixmap = QPixmap(self.map_file)
-        self.lbl.setPixmap(self.pixmap)
-        print(response.url)
+        self.photoLbl.setPixmap(self.pixmap)
+
+        # print(response.url)
 
     def closeEvent(self, event):
         os.remove(self.map_file)
@@ -56,32 +63,35 @@ class Example(QMainWindow):
                 self.z += 1
                 self.getImage()
         elif event.key() == QtCore.Qt.Key.Key_Up:
-            self.lat += 90 / 2 ** self.z
+            self.lat += 90 / 2**self.z
             if self.lat >= 85:
                 self.lat = -85
             self.getImage()
         elif event.key() == QtCore.Qt.Key.Key_Down:
-            self.lat -= 90 / 2 ** self.z
+            self.lat -= 90 / 2**self.z
             if self.lat <= -85:
                 self.lat = 85
             self.getImage()
         elif event.key() == QtCore.Qt.Key.Key_Left:
-            self.lon -= 180 / 2 ** self.z
+            self.lon -= 180 / 2**self.z
             if self.lon <= -180:
                 self.lon = 180
             self.getImage()
         elif event.key() == QtCore.Qt.Key.Key_Right:
-            self.lon += 180 / 2 ** self.z
+            self.lon += 180 / 2**self.z
             if self.lon >= 180:
                 self.lon = -180
             self.getImage()
-        elif event.key() == QtCore.Qt.Key.Key_Enter:
+        elif (
+            event.key() == QtCore.Qt.Key.Key_Return
+            or event.key() == QtCore.Qt.Key.Key_Enter
+        ):
             self.get_coords()
+            self.setFocus()
 
     def change_theme(self):
         self.theme = "light" if self.theme == "dark" else "dark"
         self.getImage()
-        self.setFocus()
 
     def get_coords(self):
         place = self.searchEdit.text()
@@ -96,16 +106,14 @@ class Example(QMainWindow):
             result = response.json()["response"]["GeoObjectCollection"][
                 "featureMember"
             ][0]["GeoObject"]["Point"]["pos"]
+            self.adress = response.json()["response"]["GeoObjectCollection"][
+                "featureMember"
+            ][0]["GeoObject"]["metaDataProperty"]["GeocoderMetaData"]["text"]
             self.lon, self.lat = map(float, result.split())
             self.search = (self.lon, self.lat)
-            self.getImage()
         except Exception:
-            pass
-        self.setFocus()
-
-    def mousePressEvent(self, event):
-        if event.pos() not in self.searchEdit.rect():
-            self.setFocus()
+            self.search = ()
+        self.getImage()
 
     def reset(self):
         self.search = ()
